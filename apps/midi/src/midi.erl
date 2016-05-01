98fa60c2ed6634f24150b1a289da141da63b031b
Tim.Tang <tang.jilong@139.com>
2016-05-01 14:13:54 +0800

    [midi] - Add notes.

--- apps/midi/src/midi.erl
+++ apps/midi/src/midi.erl
@@ -11,9 +11,18 @@
 
 %% Public API
 
+-define(SERVICE, midi).
+
 %% @doc Pings a random vnode to make sure communication is functional
 ping() ->
+    %This function is used to hash a (routing) key. It returns an integer between 0 and 2^160 - 1.
     DocIdx = riak_core_util:chash_key({<<"ping">>, term_to_binary(os:timestamp())}),
-    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, midi),
+    %DocIdx = riak_core_util:chash_key({<<"TT">>, <<"ping">>}),
+    lager:info("Routing Key => ~p~n", [DocIdx]),
+    %apl => active preference list.
+    %Vnode Type: primary/fallback
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, ?SERVICE),
+    lager:info("Preference List => ~p~n", [PrefList]),
     [{IndexNode, _Type}] = PrefList,
+    %{Partitionid, Node} = IndexNode
     riak_core_vnode_master:sync_spawn_command(IndexNode, ping, midi_vnode_master).
